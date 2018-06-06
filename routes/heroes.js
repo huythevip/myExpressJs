079b01c8618aca5868eb78de968233d3b5ab1294
TRAN QUANG HUY <quanghuy@CV08003D-PC.cubevn.local>
2018-06-06 17:46:43 +0700

    Second Commit

--- routes/heroes.js
+++ routes/heroes.js
@@ -11,7 +11,7 @@ var app = express();
 
 connection.connect()
 
-router.use('/', function (req, res, next) {
+router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -20,12 +20,41 @@ router.use('/', function (req, res, next) {
 
 /* GET home page. */
  router.get('/', function(req, res, next) {
-   connection.query('SELECT * FROM tbl_hero;', function(err, rows, fields) {
+    connection.query('SELECT * FROM tbl_hero;', function(err, rows, fields) {
        if (err) throw err
-
        res.json(rows);
-   })
+    })
 
  });
+
+ router.post('/create', function (req, res, next) {
+     // var name = req.body.heroData
+     console.log('123');
+     // connection.query(`INSERT INTO tbl_hero(name) VALUES ('${name}') `, function(err, rows, fields) {
+     //     if (err) throw err
+     //     res.send('Successfully created new Hero!');
+     // })
+ })
+
+ router.get('/:id', function(req, res, next) {
+     var id = req.params.id
+    //  res.send(id);
+     connection.query(`SELECT * FROM tbl_hero WHERE id = ${id}`, function(err, rows, fields) {
+         if (err) throw err
+         res.json(rows[0]);
+     })
+ })
+
+router.post('/:id', function (req, res, next) {
+    var id = req.body.id
+    var name = req.body.name
+    connection.query(`UPDATE tbl_hero SET name = '${name}' WHERE id = ${id}`, function (err, rows, fields) {
+        if (err) throw err
+        res.send('Successfully updated!');
+    })
+})
+
+
+
 
 module.exports = router;
